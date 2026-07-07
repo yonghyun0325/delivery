401c723e991665ce3677b81812347861b6a03a7e
daengae <dladmsxor434@naver.com>
2026-07-07 17:26:07 +0900

    feat: BaseCreatedEntity 추가

---
+++ src/main/java/com/delivery/common/base/BaseCreatedEntity.java
@@ -0,0 +1,24 @@
+package com.delivery.common.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseCreatedEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(name = "created_by", length = 100, updatable = false)
+    private String createdBy;
+}
