31aa808fe97767c1eb548902f683ddb82eb7e3d7
daengae <dladmsxor434@naver.com>
2026-07-07 17:25:09 +0900

    fix (공통): BaseEntity에 isDeleted() 및 @CreatedBy/@LastModifiedBy 어노테이션 복구

--- src/main/java/com/delivery/common/base/BaseEntity.java
+++ src/main/java/com/delivery/common/base/BaseEntity.java
@@ -5,7 +5,9 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -25,14 +27,20 @@ public abstract class BaseEntity {
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
 
-    @Column(name = "created_by", length = 100)
+    @CreatedBy
+    @Column(name = "created_by", length = 100, updatable = false)
     private String createdBy;
 
+    @LastModifiedBy
     @Column(name = "updated_by", length = 100)
     private String updatedBy;
 
     @Column(name = "deleted_by", length = 100)
     private String deletedBy;
+
+    public boolean isDeleted() {
+        return deletedAt != null;
+    }
 
     /**
      * Soft Delete 처리
