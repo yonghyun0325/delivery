c9408a52614694f329c76490b58247d52ffe2654
summmmmmmin <jsoomin01@naver.com>
2026-07-07 15:27:56 +0900

    feat : Category, Region Repository 수정

--- src/main/java/com/delivery/domain/store/repository/CategoryRepository.java
+++ src/main/java/com/delivery/domain/store/repository/CategoryRepository.java
@@ -3,7 +3,10 @@ package com.delivery.domain.store.repository;
 import com.delivery.domain.store.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
+    boolean existsByName(String name);
+    List<Category> findAllByDeletedAtIsNull();
 }
--- src/main/java/com/delivery/domain/store/repository/RegionRepository.java
+++ src/main/java/com/delivery/domain/store/repository/RegionRepository.java
@@ -3,7 +3,10 @@ package com.delivery.domain.store.repository;
 import com.delivery.domain.store.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface RegionRepository extends JpaRepository<Region, UUID> {
+    boolean existsByName(String name);
+    List<Region> findAllByDeletedAtIsNull();
 }
