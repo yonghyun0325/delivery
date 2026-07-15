95dce16672c5cab4a325c3b0c058552660a48d78
Ye-jee <yejee0408@gmail.com>
2026-07-15 14:20:19 +0900

    fix(Order): 검증 메시지가 클라이언트에 전달 오류 해결

--- src/main/java/com/delivery/global/exception/ErrorCodeRegistry.java
+++ src/main/java/com/delivery/global/exception/ErrorCodeRegistry.java
@@ -3,6 +3,7 @@ package com.delivery.global.exception;
 import com.delivery.domain.ai.exception.AiErrorCode;
 import com.delivery.domain.cart.exception.CartErrorCode;
 import com.delivery.domain.menu.exception.MenuErrorCode;
+import com.delivery.domain.order.exception.OrderErrorCode;
 import com.delivery.domain.user.exception.AuthErrorCode;
 import com.delivery.domain.user.exception.UserErrorCode;
 import com.delivery.domain.store.exception.StoreErrorCode;
@@ -22,6 +23,7 @@ public class ErrorCodeRegistry {
         register(CartErrorCode.values());
         register(AiErrorCode.values());
         register(StoreErrorCode.values());
+        register(OrderErrorCode.values());
     }
 
     private void register(ErrorCode[] codes) {
