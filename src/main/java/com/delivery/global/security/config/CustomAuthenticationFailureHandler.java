301117b2e1370f959baad1d2e92ab5339762396f
cpfur18 <cpfur18@gmail.com>
2026-07-19 09:47:26 +0900

    feat: 로그인 실패 핸들러 추가

---
+++ src/main/java/com/delivery/global/security/config/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,34 @@
+package com.delivery.global.security.config;
+
+import com.delivery.common.RestApiResponse;
+import com.delivery.domain.user.exception.AuthErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        AuthErrorCode errorCode = AuthErrorCode.INVALID_LOGIN;
+
+        response.setStatus(errorCode.getHttpStatus().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+
+        var authResponse =
+                RestApiResponse.fail(
+                        errorCode.getHttpStatus(), errorCode.getMessage(), errorCode.getName());
+
+        objectMapper.writeValue(response.getWriter(), authResponse);
+    }
+}
