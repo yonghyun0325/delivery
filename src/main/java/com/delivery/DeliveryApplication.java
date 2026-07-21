715fd201f4dc12a741abb478df97808c19321bae
cpfur18 <cpfur18@gmail.com>
2026-07-22 04:20:52 +0900

    [build] : annotationProcessor 'org.springframework.boot:spring-boot-configuration-processor' 추가 - @ConfigurationProperties 사용 시 yml또는 properties에 IDE 자동 완성 기능을 사용하기 위함

--- src/main/java/com/delivery/DeliveryApplication.java
+++ src/main/java/com/delivery/DeliveryApplication.java
@@ -2,7 +2,6 @@ package com.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
