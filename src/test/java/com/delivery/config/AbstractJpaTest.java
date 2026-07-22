90ebaeaf88e8e0e9efcf2c11907e7d06bccc45f1
cpfur18 <cpfur18@gmail.com>
2026-07-22 17:44:10 +0900

    [test] : redis 변경에 따라 DataJpaTest의 경우 기존의 AbstractIntegrationTest 상속불가. - Redis에 대한 의존성을 제거한 AbstractJpaTest 클래스 생성

---
+++ src/test/java/com/delivery/config/AbstractJpaTest.java
@@ -0,0 +1,26 @@
+package com.delivery.config;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class AbstractJpaTest {
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:17")
+                    .withDatabaseName("testdb")
+                    .withUsername("test")
+                    .withPassword("test");
+
+    static {
+        postgres.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+}
