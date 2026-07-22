818e72190425b98ff626fe751862928198b8a921
cpfur18 <cpfur18@gmail.com>
2026-07-22 19:09:03 +0900

    [chore] : Properties recode로 변경

--- src/main/java/com/delivery/global/security/config/EncryptProperties.java
+++ src/main/java/com/delivery/global/security/config/EncryptProperties.java
@@ -1,16 +1,6 @@
 package com.delivery.global.security.config;
 
-import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@Getter
 @ConfigurationProperties(prefix = "encrypt")
-public class EncryptProperties {
-    private final String key;
-    private final String salt;
-
-    public EncryptProperties(String key, String salt) {
-        this.key = key;
-        this.salt = salt;
-    }
-}
+public record EncryptProperties(String key, String salt) {}
