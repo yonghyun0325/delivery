92a35534d867b85f2330d4b015d0a8f2db597018
cpfur18 <cpfur18@gmail.com>
2026-07-28 02:53:38 +0900

    [chore] : 주석제거

--- src/main/java/com/delivery/common/base/BaseEntity.java
+++ src/main/java/com/delivery/common/base/BaseEntity.java
@@ -16,9 +16,6 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-/*@Getter
-@MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)*/
 @Getter
 @SuperBuilder
 @MappedSuperclass
